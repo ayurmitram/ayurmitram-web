878565e71a96601cec3a03650bf99cb6631a5d75
Mohit Khairnar <mvk1407@gmail.com>
2023-12-05 00:17:03 +0530

    refactor: modified ui chatbot interface

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors:{
+        'ayurgreen': '#539C52'
+      },
       fontFamily:{
         'opensans': ['Open Sans', 'sans-serif']
       }
