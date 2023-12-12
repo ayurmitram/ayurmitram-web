b9c74baeb32abe43488b7813cfb7544a6e3ed8de
Mohit Khairnar <mvk1407@gmail.com>
2023-12-12 13:12:39 +0530

    refactor: updated doc schema

--- server/models/doctorSchema.js
+++ server/models/doctorSchema.js
@@ -13,7 +13,47 @@ const DoctorSchema = new mongoose.Schema(
         doctor_password: {
             type: String,
             required: true,
+        },
+        doctor_consultant_type:{
+            type: String
+        },
+        doctor_specialization: {
+            type: String
+        },
+        doctor_experience: {
+            type: String
+        },
+        doctor_description: {
+            type: String
+        },
+        doctor_education: {
+            type: String
+        },
+        doctor_clinic_name: {
+            type: String
+        },
+        doctor_clinic_address: {
+            type: String
+        },
+        doctor_contact_number: {
+            type: Number
+        },
+        doctor_languages_spoken: {
+            type: [String]
+        },
+        doctor_availability: {
+            type: Date
+        },
+        doctor_preferred_comm: {
+            type: [String]
+        },
+        doctor_area_of_expertise: {
+            type: [String]
+        },
+        doctor_website: {
+            type: String
         }
+
     }
 )
 
