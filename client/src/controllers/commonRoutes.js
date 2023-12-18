7c4f9f4e9f57be0d670f287e8480608c6ff1cb73
Satyam Sagar <satyamsagar1711@gmail.com>
2023-12-18 23:47:44 +0530

    missed file

---
+++ client/src/controllers/commonRoutes.js
@@ -0,0 +1,27 @@
+const baseUrl = "http://localhost:8000";
+
+export const login_common = async(obj) => {
+    const res = await fetch(`${baseUrl}/api/login/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    })
+
+    const ans = await res.json();
+    return ans;
+}
+
+export const auth_common = async(obj) => {
+    const res = await fetch(`${baseUrl}/api/login/auth`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    })
+
+    const ans = await res.json();
+    return ans;
+}
