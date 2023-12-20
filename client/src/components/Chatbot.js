f8264d4bfd8d537f9a2250290572b3884eae4c18
HarshJa1n <hjain012003@gmail.com>
2023-12-20 12:55:29 +0530

    [v1] invite to signup & basic auth

--- client/src/components/Chatbot.js
+++ client/src/components/Chatbot.js
@@ -19,137 +19,8 @@ import SpeechRecognition, {
 } from "react-speech-recognition";
 import { auth_patient } from "../controllers/patientRoutes";
 // import MyDocument from "./PdfReport";
-import {
-  Document,
-  Page,
-  View,
-  Text,
-  PDFDownloadLink,
-  StyleSheet
-} from '@react-pdf/renderer';
-
-
-
-const styles = StyleSheet.create({
-  page: {
-    padding: 30
-  },
-  title: {
-    fontSize: 30,
-    textAlign: 'center',
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 15,
-    marginBottom: 10
-  },
-  text: {
-    fontSize: 12,
-    marginBottom: 5
-  },
-  table: {
-    display: 'table',
-    width: 'auto',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderRightWidth: 0,
-    borderBottomWidth: 0
-  },
-  tableRow: {
-    margin: 'auto',
-    flexDirection: 'row'
-  },
-  tableCol1Header: {
-    width: '20%',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderLeftWidth: 0,
-    borderTopWidth: 0
-  },
-  tableCol2Header: {
-    width: '60%',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderLeftWidth: 0,
-    borderTopWidth: 0
-  },
-  tableCol3Header: {
-    width: '20%',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderLeftWidth: 0,
-    borderTopWidth: 0
-  },
-  tableCol1: {
-    width: '20%',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderLeftWidth: 0,
-    borderTopWidth: 0
-  },
-  tableCol2: {
-    width: '60%',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderLeftWidth: 0,
-    borderTopWidth: 0
-  },
-  tableCol3: {
-    width: '20%',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderLeftWidth: 0,
-    borderTopWidth: 0
-  }
-});
-
-const MyDocument = () => {
-  const patientName = 'John Doe';
-  const patientAge = 25;
-  // const patientGender = "MALE"
-  const selectedResponses = useSelector((state) => state.layout.selectedResponses);
-
-  return (
-
-    <Document>
-      <Page size="A4" style={styles.page}>
-        <Text style={styles.title}>AyurMitram Report</Text>
-
-        {/* Patient Details */}
-        <Text style={styles.subtitle}>Patient Details</Text>
-        <Text style={styles.text}>Name: {patientName}</Text>
-        <Text style={styles.text}>Age: {patientAge}</Text>
-        {/* <Text style={styles.text}>Gender: {patientGender}</Text> */}
-
-        {/* Analysis */}
-        <Text style={styles.subtitle}>Prakruti Analysis</Text>
-
-        <View style={styles.table}>
-          <View style={styles.tableRow}>
-            <Text style={styles.tableCol1Header}>No.</Text>
-            <Text style={styles.tableCol2Header}>Question</Text>
-            <Text style={styles.tableCol3Header}>Response</Text>
-          </View>
-
-          {selectedResponses.map((res, index) => (
-            <View style={styles.tableRow} key={index}>
-              <Text style={[styles.tableCol1]}>{index + 1}</Text>
-              <Text style={[styles.tableCol2]}>
-                {res.type === 'bot' ?
-                  JSON.parse(res.text?.answer || res.text)?.question : ''}
-              </Text>
-              <Text style={[styles.tableCol3]}>
-                {res.display || res.text}
-              </Text>
-            </View>
-          ))}
-
-        </View>
-
-      </Page>
-    </Document>
-  )
-};
+import { PDFDownloadLink, Document, Page } from '@react-pdf/renderer';
+import MyDocument from "./PdfReport";
 
 const MessageBox = ({ message, prev, next, handleSendMessage }) => {
   const selectedResponses = useSelector((state) => state.layout.selectedResponses);
@@ -682,7 +553,7 @@ const Chatbot = () => {
             >
               Generate PDF
             </Button>
-            <PDFDownloadLink document={<MyDocument />} fileName="somename.pdf">
+            <PDFDownloadLink document={<MyDocument selectedResponses={selectedResponses} patientDetails={patientDetails} />} fileName="somename.pdf">
               {({ blob, url, loading, error }) =>
                 loading ? 'Loading document...' : 'Download now!'
               }
--- client/src/components/PdfReport.js
+++ client/src/components/PdfReport.js
@@ -3,102 +3,61 @@ import {
     Page,
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Image
 } from '@react-pdf/renderer';
+import { Chart } from 'chart.js';
 import { Provider, useSelector } from 'react-redux';
 
 
 
-const styles = StyleSheet.create({
-    page: {
-        padding: 30
-    },
-    title: {
-        fontSize: 30,
-        textAlign: 'center',
-        marginBottom: 10,
-    },
-    subtitle: {
-        fontSize: 15,
-        marginBottom: 10
-    },
-    text: {
-        fontSize: 12,
-        marginBottom: 5
-    },
-    table: {
-        display: 'table',
-        width: 'auto',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderRightWidth: 0,
-        borderBottomWidth: 0
-    },
-    tableRow: {
-        margin: 'auto',
-        flexDirection: 'row'
-    },
-    tableCol1Header: {
-        width: '20%',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderLeftWidth: 0,
-        borderTopWidth: 0
-    },
-    tableCol2Header: {
-        width: '60%',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderLeftWidth: 0,
-        borderTopWidth: 0
-    },
-    tableCol3Header: {
-        width: '20%',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderLeftWidth: 0,
-        borderTopWidth: 0
-    },
-    tableCol1: {
-        width: '20%',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderLeftWidth: 0,
-        borderTopWidth: 0
-    },
-    tableCol2: {
-        width: '60%',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderLeftWidth: 0,
-        borderTopWidth: 0
-    },
-    tableCol3: {
-        width: '20%',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderLeftWidth: 0,
-        borderTopWidth: 0
-    }
-});
 
-const MyDocument = () => {
-    const patientName = 'John Doe';
-    const patientAge = 25;
-    // const patientGender = "MALE"
-    const selectedResponses = useSelector((state) => state.layout.selectedResponses);
+
+const MyDocument = ({ selectedResponses, patientDetails }) => {
+    const { name, age, gender, medical_history } = patientDetails;
+    const lastEntry = medical_history.length > 0 ? medical_history[medical_history.length - 1] : null;
+    const lineHeight = 12;
+    let currentY = 50;
+
+    console.log(selectedResponses, 'selectedResponses')
+    console.log(patientDetails, 'patientDetails')
 
     return (
 
         <Document>
             <Page size="A4" style={styles.page}>
+                <View style={styles.legend}>
+
+                </View>
                 <Text style={styles.title}>AyurMitram Report</Text>
 
                 {/* Patient Details */}
                 <Text style={styles.subtitle}>Patient Details</Text>
-                <Text style={styles.text}>Name: {patientName}</Text>
-                <Text style={styles.text}>Age: {patientAge}</Text>
-                {/* <Text style={styles.text}>Gender: {patientGender}</Text> */}
+                <Text style={styles.text}>Name: {name}</Text>
+                <Text style={styles.text}>Age: {age}</Text>
+                <Text style={styles.text}>Gender: {gender}</Text>
+                <View>
+                    {lastEntry && (
+                        <>
+                            <Text style={{ marginTop: currentY - 3 * lineHeight }}>
+                                Blood Pressure Level: High - {lastEntry.blood_pressure.high} mmHg, Low - {lastEntry.blood_pressure.low} mmHg
+                            </Text>
+                            <Text style={{ marginTop: lineHeight }}>
+                                Sugar Level: Before Food - {lastEntry.sugar_level.before_food} mg/dL, After Food - {lastEntry.sugar_level.after_food} mg/dL
+                            </Text>
+                            <Text style={{ marginTop: lineHeight }}>
+                                Pulse Rate: {lastEntry.pulse_rate} bpm
+                            </Text>
+                            <Text style={{ marginTop: lineHeight }}>
+                                Temperature Recorded: {lastEntry.temperature} °F
+                            </Text>
+                            <Text style={{ marginTop: lineHeight }}>
+                                Sleep Hours Noted: {lastEntry.sleep_hours} hrs
+
+                            </Text>
+                        </>
+                    )}
+                </View>
 
                 {/* Analysis */}
                 <Text style={styles.subtitle}>Prakruti Analysis</Text>
@@ -110,18 +69,22 @@ const MyDocument = () => {
                         <Text style={styles.tableCol3Header}>Response</Text>
                     </View>
 
-                    {selectedResponses.map((res, index) => (
-                        <View style={styles.tableRow} key={index}>
-                            <Text style={[styles.tableCol1]}>{index + 1}</Text>
-                            <Text style={[styles.tableCol2]}>
-                                {res.type === 'bot' ?
-                                    JSON.parse(res.text?.answer || res.text)?.question : ''}
-                            </Text>
-                            <Text style={[styles.tableCol3]}>
-                                {res.display || res.text}
-                            </Text>
-                        </View>
-                    ))}
+                    {selectedResponses?.map((res, index) => {
+                        console.log(res, 'res')
+                        return (
+                            <View style={styles.tableRow} key={index}>
+                                <Text style={[styles.tableCol1]}>{index + 1}</Text>
+                                <Text style={[styles.tableCol2]}>
+                                    {res.type === 'bot' ?
+                                        JSON.parse(res.text?.answer || res.text)?.question : ''}
+                                </Text>
+                                <Text style={[styles.tableCol3]}> sdf </Text>
+                                <Text style={[styles.tableCol3]}>
+                                    {res.display || res.text}
+                                </Text>
+                            </View>
+                        )
+                    })}
 
                 </View>
 
@@ -132,3 +95,62 @@ const MyDocument = () => {
 
 export default MyDocument;
 
+
+const styles = StyleSheet.create({
+    page: {
+        padding: 10, // Adjust the padding
+    },
+    title: {
+        fontFamily: 'Poppins',
+        fontSize: 20,
+        color: 'green',
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    subtitle: {
+        fontFamily: 'Poppins',
+        fontSize: 15,
+        color: 'black',
+        marginBottom: 10
+    },
+    text: {
+        fontFamily: 'Poppins',
+        fontSize: 12,
+        color: 'black',
+        marginBottom: 5
+    },
+    table: {
+        display: 'table',
+        width: 'auto',
+        borderStyle: 'none', // Remove the border
+    },
+    tableRow: {
+        margin: 'auto',
+        flexDirection: 'row'
+    },
+    tableCol1Header: {
+        width: '20%',
+    },
+    tableCol2Header: {
+        width: '60%',
+    },
+    tableCol3Header: {
+        width: '20%',
+    },
+    tableCol1: {
+        width: '10%',
+    },
+    tableCol2: {
+        width: '60%',
+    },
+    tableCol3: {
+        width: '30%',
+    },
+    legend: {
+        display: 'flex',
+        backgroundColor: 'green',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+    },
+});
